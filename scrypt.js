4dba5e3cf35c19c2fdb092d50f4c8fdef379dd58
Mjogan <wow.mjogan.official@yandex.ru>
2020-12-16 21:07:47 +0300

    добавление лайков

--- scrypt.js
+++ scrypt.js
@@ -5,3 +5,7 @@ document.getElementById('edit-button').onclick = function() {
 document.getElementById('popup__button-close').onclick = function() {
   document.getElementById('popup').classList.remove('popup__open');
 }
+
+// document.getElementById('like').onclick = function() {
+//   document.getElementById('like').classList.toggle('like_active');
+// }
