98f9da7c60f47032d53cf093443bd12273e82be3
Ivan Minin <wow.mjogan.official@yandex.ru>
2021-02-20 13:59:15 +0300

    сделать классы

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,8 @@
+// Импорты
+import Card from './card.js';
+
 // Переменные
 
-// создаем объект с карточками
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
-
-// перевернем массив
-const cardsReverse = initialCards.reverse();
 // селекторы страницы
 const root = document.querySelector('.root');
 const elements = root.querySelector('.elements');
@@ -136,7 +109,7 @@ const renderCard = cardElement => {
 }
 
 // выводим карточки
-cardsReverse.forEach(renderCard);
+// cardsReverse.forEach(renderCard);
 
 // закрытие на оверлей
 const closePopupOverlay = () => {
